d48e95cbff5022b156daac98d7945c081c87d864
Alexander Koroclhuk <alkor1@yandex.ru>
2012-11-21 01:40:46 +0300

    Add textures

--- Smail platform/Assets/Scripts/HeroScript.cs
+++ Smail platform/Assets/Scripts/HeroScript.cs
@@ -21,7 +21,13 @@ public class HeroScript : MonoBehaviour {
 	public float StartJumpTime;
 	public float FinishJumpTime;
 	public bool OnGround;
-	public float JumpTimeOut;
+	public float JumpTimeOut = 2;
+	public float LastJumpTime = float.MinValue;
+	
+	public Material heroMaterial;
+	public float LastChangeFrame = float.MinValue;
+	public float TimeOutToChangeFrame;
+	public float BackVerticalSpeed;
 	
 	public bool IsLookAtRight = true;
 	// Use this for initialization
@@ -33,14 +39,14 @@ public class HeroScript : MonoBehaviour {
 	{
 	 IsForwardMoving = false;
 	 IsJumpStart = false;
-	 if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) && !IsForwardMovingInProcess)
+	 if((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !IsForwardMovingInProcess)
 		{
 			IsLookAtRight = true;
 			IsForwardMoving = true;			
 			CurrentHorizontalSpeed = Mathf.Abs(HorizontalStepSpeed);
 			CurrentVerticalStepSpeed = Mathf.Abs(VerticalStepSpeed);
 		}
-	 if(Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow) && !IsForwardMovingInProcess)
+	 if((Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) && !IsForwardMovingInProcess)
 		{
 			IsForwardMoving = true;
 			IsLookAtRight = false;
@@ -48,7 +54,7 @@ public class HeroScript : MonoBehaviour {
 			CurrentVerticalStepSpeed = Mathf.Abs(VerticalStepSpeed);	
 		}
 			
-	 if(Input.GetKey(KeyCode.W) || Input.GetKey (KeyCode.UpArrow) && OnGround && !IsJumpInProgress )
+	 if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && OnGround && !IsJumpInProgress && Time.time-LastJumpTime>JumpTimeOut)
 		{
 			IsJumpStart = true;
 			CurrentVerticalStepSpeed = 0;
@@ -68,7 +74,13 @@ public class HeroScript : MonoBehaviour {
 		}
 	if(IsForwardMovingInProcess )
 		{
-			if(Time.time - StartTimeOfForwardStep<ForwardStepTimeDuration)
+			if(Time.time - LastChangeFrame > TimeOutToChangeFrame)
+			{
+				LastChangeFrame = Time.time;
+				var offset = heroMaterial.mainTextureOffset;
+				heroMaterial.mainTextureOffset =new Vector2(offset.x+0.25f,offset.y);
+			}
+			if(Time.time - StartTimeOfForwardStep < ForwardStepTimeDuration)
 				{
 					transform.localPosition+=new Vector3(CurrentHorizontalSpeed,CurrentVerticalStepSpeed,0)*Time.deltaTime;
 				}
@@ -88,12 +100,15 @@ public class HeroScript : MonoBehaviour {
 		{
 			IsJumpInProgress = true;
 			StartJumpTime = Time.time;
+			LastJumpTime = Time.time;
+			BackVerticalSpeed = 0;
 		}
 		if(IsJumpInProgress)
 		{
 			if(Time.time - StartJumpTime <= JumpDurationTime)
 			{
-				transform.localPosition+=new Vector3(0,CurrentVerticalSpeed,0)*Time.deltaTime;
+				transform.localPosition+=new Vector3(0,CurrentVerticalSpeed,0)
+					*Time.deltaTime*((JumpDurationTime-(Time.time-StartJumpTime))/JumpDurationTime);
 			}
 			else
 			{
@@ -108,7 +123,8 @@ public class HeroScript : MonoBehaviour {
 		{
 			if(!IsJumpInProgress)
 			{
-				transform.localPosition+=new Vector3(0,-CurrentVerticalSpeed,0)*Time.deltaTime;
+				BackVerticalSpeed+=(400f*Time.deltaTime);
+				transform.localPosition+=new Vector3(0,-BackVerticalSpeed,0)*Time.deltaTime;
 			}
 		}
 	}
@@ -119,7 +135,7 @@ public class HeroScript : MonoBehaviour {
 	}*/
 	void OnTriggerExit(Collider other)
 	{
-		if((other.gameObject.tag == "Floor"))
+		if((other.gameObject.tag == "MainFloor") || ((other.gameObject.tag == "Floor")))
 		{
 			OnGround = false;
 		}
@@ -134,6 +150,12 @@ public class HeroScript : MonoBehaviour {
 			{
 				OnGround = true;
 			}
+			IsForwardMovingInProcess = false;
+			IsJumpInProgress = false;
+		}
+		if(other.gameObject.tag == "MainFloor")
+		{
+			OnGround = true;
 			IsForwardMovingInProcess = false;
 			IsJumpInProgress = false;
 		}
